6f0b970052a4fedd2b8e3716e82095751b5fca39
Jenn Bowers <jenniferbowers03@gmail.com>
2017-07-28 10:47:00 -0400

    Began project and put template into intelliJ

---
+++ src/main/java/com/jennbowers/bowersportfolio/BowersportfolioApplication.java
@@ -0,0 +1,12 @@
+package com.jennbowers.bowersportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BowersportfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BowersportfolioApplication.class, args);
+	}
+}
